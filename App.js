a116c08b3ea3478a6c0d24f43605cb2465ab2988
Prashant-SK <psk.dev.ca@gmail.com>
2020-09-16 16:11:09 -0700

    added image and clickable text on homesceen

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableWithoutFeedback,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed");
   return (
-    <View style={styles.container}>
-      <Text>Hello Cars</Text>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>Hello Cars</Text>
+      <Image
+        blurRadius={10}
+        fadeDuration={1000}
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
